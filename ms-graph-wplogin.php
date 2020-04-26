1c98409fc65101a66b5dc4f0940944532f71867b
Russell Ramey <doublercreative@gmail.com>
2020-04-26 13:45:50 -0500

    added plugin meta and details

---
+++ ms-graph-wplogin.php
@@ -0,0 +1,8 @@
+<?php
+/***
+* Plugin Name: MS Graph WP Login
+* Description: Enable Wordpress login via Microsoft Graph API using an enterprise Tennent ID and Active Directory.
+* Version: 1.0
+* Author: Russell Ramey
+* Author URI: https://russellramey.dev/
+***/
